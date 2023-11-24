4fa616ad1babc61841855f4a51455e41cfc07323
yao <838535297@qq.com>
2023-11-24 19:53:22 +0800

    无数据

--- src/components/noData/NoData.vue
+++ src/components/noData/NoData.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { getImageURL } from '@/utils'
+import { onBeforeMount, ref } from 'vue'
 
 const props = defineProps({
   img: {
@@ -11,10 +12,16 @@ const props = defineProps({
     default: '暂无相关数据哦'
   }
 })
+const isShow = ref(false)
+onBeforeMount(() => {
+  setTimeout(() => {
+    isShow.value = true
+  }, 500)
+})
 </script>
 
 <template>
-  <view class="fixed w-100% h-100vh flex justify-center items-center flex-col">
+  <view class="fixed w-100% h-100vh flex justify-center items-center flex-col" v-if="isShow">
     <u-image :show-loading="true" :src="getImageURL('empty', props.img)" width="80vw" />
     <view class="mt-200rpx text-#BFCEE0 text-[18px]">{{ props.text }}</view>
   </view>
