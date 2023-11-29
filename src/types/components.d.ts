0262cfd69921c198ea6fbf7cb8c869aa3471ea1d
pxn <418070016@qq.com>
2023-11-29 18:07:06 +0800

    调整细节

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -1,6 +1,7 @@
 declare module 'vue' {
   export interface GlobalComponents {
     ContentWrap: typeof import('@/components/ContentWrap/index.vue')
+    Header: typeof import('@/components/Header/index.vue')
   }
 }
 
