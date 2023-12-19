47f24102ab6700d5b286136f9912861fc6d67a3f
yao <838535297@qq.com>
2023-12-19 14:20:12 +0800

    看房预约

--- src/api/modules/welfare.ts
+++ src/api/modules/welfare.ts
@@ -6,3 +6,31 @@ const { post } = useAxios()
 export const ProductListApi = (data: any) => {
   return post<any>('product/list', { ...data })
 }
+
+/**看房预约列表 */
+export const HouseReservationList = (data: any) => {
+  console.log('data', data)
+  return post<any>('house-reservation/list', { ...data })
+}
+
+/**房屋列表 */
+export const HouseList = (data: any, tabActive: number) => {
+  if (tabActive === 1) {
+    return post<any>('house/list', { ...data })
+  } else {
+    return post<any>('house-reservation/list', { ...data })
+  }
+}
+/**房屋列表 */
+export const HouseInfo = (houseId: any) => {
+  return post<any>('house/info', { houseId })
+}
+
+/**看房预约添加编辑 */
+export const HouseReservationUpdate = (data: any, houseReservationId: string) => {
+  if (houseReservationId) {
+    return post<any>('house-reservation/update', { ...data, houseReservationId })
+  } else {
+    return post<any>('house-reservation/add', { ...data })
+  }
+}
--- src/pages/customTabbar/src/Serve.vue
+++ src/pages/customTabbar/src/Serve.vue
@@ -184,6 +184,9 @@ const onJump = (title: string) => {
     case '电子发票':
       src = `/packageHome/parkingFee/ElectronicInvoice`
       break
+    case '看房预约':
+      src = `/packageHome/viewingAppointment/index`
+      break
     default:
       uni.showToast({
         title: '待开发',
